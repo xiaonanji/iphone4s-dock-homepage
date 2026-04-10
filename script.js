0f5a490129aa1c45887abd05101fc9ed7af208ea
xiaonanji <sticker.ji@gmail.com>
2026-04-10 22:08:16 +1000

    Use much taller landscape panel ratio

--- script.js
+++ script.js
@@ -41,8 +41,8 @@
       isPortrait = viewportHeight > viewportWidth;
     }
 
-    var baseWidth = isPortrait ? 296 : 456;
-    var baseHeight = isPortrait ? 432 : 228;
+    var baseWidth = isPortrait ? 296 : 420;
+    var baseHeight = isPortrait ? 432 : 268;
     var horizontalPadding = isPortrait ? 16 : 4;
     var verticalPadding = isPortrait ? 18 : 4;
     var scaleX = (viewportWidth - horizontalPadding) / baseWidth;
